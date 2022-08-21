7291e42b9ae7d888502724b4e2555455ce105826
SHAHIN <70063923+shahinfn@users.noreply.github.com>
2022-08-21 16:32:58 +0600

    Japan is My Favorite Country

--- first.php
+++ first.php
@@ -1,10 +1,9 @@
 <?php
 
-  echo " php first code ";
-  echo "php Second code";
-  echo "Edit PHP code";
-  echo "Bangladesh";
-  echo "Japan";
-  echo "Japan is a most powerfull country in Tech World";
+  echo " Bangladesh ";
+  echo " Pkistan ";
+  echo " Afgamostan ";
+  echo " Canada ";
+  echo " Japan ";
 
 ?>
